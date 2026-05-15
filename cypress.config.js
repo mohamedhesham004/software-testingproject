38b3359f94a90194cb38a8456f9d0bacdc9d096b
mohamedhesham004 <mohamedhesham0200@gmail.com>
2026-05-15 20:35:19 +0300

    Resolve Node.js 20 warning and fix cypress.config.js syntax

--- cypress.config.js
+++ cypress.config.js
@@ -5,13 +5,16 @@ const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esb
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://practicesoftwaretesting.com',
     specPattern: [
       'cypress/e2e/**/*.cy.js',
       'cypress/e2e/features/**/*.feature'
     ],
     viewportWidth: 1280,
     viewportHeight: 720,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
     chromeWebSecurity: false,
+    failOnStatusCode: false,
     experimentalModifyObstructiveThirdPartyCode: true,
     async setupNodeEvents(on, config) {
       // Required for Cucumber preprocessor
