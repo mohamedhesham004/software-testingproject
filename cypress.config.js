34101df731d9b2848c13cc97b5c4bcd3c47a1ce6
mohamedhesham004 <mohamedhesham0200@gmail.com>
2026-05-15 23:27:50 +0300

    Fixed Cypress env configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,48 @@
 const { defineConfig } = require('cypress');
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
-const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
-const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
+
+const createBundler =
+require('@bahmutov/cypress-esbuild-preprocessor');
+
+const addCucumberPreprocessorPlugin =
+require('@badeball/cypress-cucumber-preprocessor')
+.addCucumberPreprocessorPlugin;
+
+const createEsbuildPlugin =
+require('@badeball/cypress-cucumber-preprocessor/esbuild')
+.createEsbuildPlugin;
 
 module.exports = defineConfig({
+
+  allowCypressEnv: true,
+
   e2e: {
+
     baseUrl: 'https://practicesoftwaretesting.com',
+
     specPattern: [
-      'cypress/e2e/features/**/*.feature'
+      'cypress/e2e/features/*/.feature'
     ],
+
     viewportWidth: 1280,
     viewportHeight: 720,
-    userAgent: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+
+    userAgent:
+    'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+
     chromeWebSecurity: false,
-    failOnStatusCode: false,
+
     experimentalModifyObstructiveThirdPartyCode: true,
+
     defaultCommandTimeout: 5000,
     requestTimeout: 5000,
     responseTimeout: 5000,
+
     video: false,
     screenshotOnRunFailure: false,
     numTestsKeptInMemory: 0,
-    allowCypressEnv: true,
+
     async setupNodeEvents(on, config) {
-      // Required for Cucumber preprocessor
+
       await addCucumberPreprocessorPlugin(on, config);
 
       on(
@@ -33,19 +52,22 @@ module.exports = defineConfig({
         })
       );
 
-      // CI specific flags
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.family === 'chromium' && browser.name !== 'electron') {
+      on('before:browser:launch',
+      (browser = {}, launchOptions) => {
+
+        if (
+          browser.family === 'chromium' &&
+          browser.name !== 'electron'
+        ) {
+
           launchOptions.args.push('--no-sandbox');
           launchOptions.args.push('--disable-gpu');
           launchOptions.args.push('--disable-dev-shm-usage');
-          launchOptions.args.push('--disable-extensions');
-          launchOptions.args.push('--disable-plugins');
+
         }
-        if (browser.name === 'electron') {
-          launchOptions.args.push('--disable-gpu');
-        }
+
         return launchOptions;
+
       });
 
       return config;
